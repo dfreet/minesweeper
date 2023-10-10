09a6f941137d58a8589fe1ade2ab74b957c603f0
Devyn Freet <devyn@devynfreet.com>
2023-10-10 14:32:30 -0400

    e

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,28 @@ import controller.GuiController;
 import view.GuiView;
 
 public class Main {
-    public static final boolean consoleMode = false;
+    enum Mode {
+        CONSOLE, GUI, WEB
+    }
+    public static final Mode defMode = Mode.GUI;
     public static final boolean debug = false;
 
     public static void main(String[] args) {
-        if (consoleMode) {
-            ConsolePlayer.play(debug);
+        Mode mode;
+        if (args.length > 0) {
+            mode = switch (args[0].toLowerCase()) {
+                case "console" -> Mode.CONSOLE;
+                case "gui" -> Mode.GUI;
+                case "web" -> Mode.WEB;
+                default -> defMode;
+            };
         } else {
+            mode = defMode;
+        }
+
+        if (mode == Mode.CONSOLE) {
+            ConsolePlayer.play(debug);
+        } else if (mode == Mode.GUI) {
             GuiView view = new GuiView();
             new GuiController(view, debug);
             view.setVisible(true);
